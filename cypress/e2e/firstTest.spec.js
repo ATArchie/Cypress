3864d81f76463d353a1c55a29adfd18b5cd982dd
Arkadiusz Tomalski <arkadiusz.tomalski@g2a.com>
2023-09-20 18:35:03 +0200

    tests cd

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -198,7 +198,7 @@ describe("Our first suite", () => {
       });
   });
 
-  it('Web tables', () => {
+  it.only('Web tables', () => {
       cy.visit("/");
       cy.contains("Tables & Data").click();
       cy.contains("Smart Table").click();
@@ -209,6 +209,8 @@ describe("Our first suite", () => {
         cy.wrap(tableRow).find('.nb-checkmark').click()
         cy.wrap(tableRow).find('td').eq(6).should('contain', '25')
       })
+
+      //1
       cy.get('thead').find('.nb-plus').click()
       cy.get('thead').find('tr').eq(2).then( tableRow => {
         cy.wrap(tableRow).find('[placeholder="First Name"]').type('A')
@@ -216,11 +218,27 @@ describe("Our first suite", () => {
         cy.wrap(tableRow).find('.nb-checkmark').click()
 
       })
+      
+      //2
       cy.get('tbody tr').first().find('td').then( tableColumns => {
         cy.wrap(tableColumns).eq(2).should('contain', 'A')
         cy.wrap(tableColumns).eq(3).should('contain', 'T')
 
       })
 
-  })
+      //3
+      const age = [20, 30, 40, 200]
+      cy.wrap(age).each(age => {
+        cy.get('thead [placeholder="Age"]').clear().type(age)
+        cy.wait(500)
+        cy.get('tbody tr').each( tableRow => {
+          if(age == 200){
+            cy.wrap(tableRow).should('contain', "No data found")
+          }else{
+            cy.wrap(tableRow).find('td').eq(6).should('contain', age)
+          }
+        })
+      })
+
+    })
 });
